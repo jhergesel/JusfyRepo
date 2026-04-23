0ddcda7b1c066f329b11a04cd36e62cfe20f54be
Josué Hergesel <josuealvez.junior@hotmail.com>
2026-04-23 14:19:40 -0300

    commit
    commit

---
+++ js.hs-analytics.net/analytics/1776963900000/47445192.js
@@ -0,0 +1,1 @@
+No Content: https://js.hs-analytics.net/analytics/1776963900000/47445192.js
--- js.hs-scripts.com/47445192.js
+++ js.hs-scripts.com/47445192.js
@@ -1,18 +1,48 @@
 // HubSpot Script Loader. Please do not block this resource. See more: http://hubs.ly/H0702_H0
 
+var _hsp = window._hsp = window._hsp || [];
+_hsp.push(['addEnabledFeatureGates', []]);
+_hsp.push(['setBusinessUnitId', 0]);
 ! function(t, e, r) {
     if (!document.getElementById(t)) {
         var n = document.createElement("script");
-        for (var a in n.src = "https://js.usemessages.com/conversations-embed.js", n.type = "text/javascript", n.id = t, r) r.hasOwnProperty(a) && n.setAttribute(a, r[a]);
+        for (var a in n.src = "https://js.hs-banner.com/v2/47445192/banner.js", n.type = "text/javascript", n.id = t, r) r.hasOwnProperty(a) && n.setAttribute(a, r[a]);
         var i = document.getElementsByTagName("script")[0];
         i.parentNode.insertBefore(n, i)
     }
-}("hubspot-messages-loader", 0, {
+}("cookieBanner-47445192", 0, {
+    "data-cookieconsent": "ignore",
+    "data-hs-ignore": true,
     "data-loader": "hs-scriptloader",
     "data-hsjs-portal": 47445192,
     "data-hsjs-env": "prod",
     "data-hsjs-hublet": "na1"
 });
+! function(t, e, r) {
+    if (!document.getElementById(t)) {
+        var n = document.createElement("script");
+        for (var a in n.src = "https://js.hsadspixel.net/pixels.js", n.type = "text/javascript", n.id = t, r) r.hasOwnProperty(a) && n.setAttribute(a, r[a]);
+        var i = document.getElementsByTagName("script")[0];
+        i.parentNode.insertBefore(n, i)
+    }
+}("hs-ads-pixel-47445192", 0, {
+    "data-ads-portal-id": 47445192,
+    "data-ads-env": "prod",
+    "data-loader": "hs-scriptloader",
+    "data-hsjs-portal": 47445192,
+    "data-hsjs-env": "prod",
+    "data-hsjs-hublet": "na1"
+});
+! function(e, t) {
+    if (!document.getElementById(e)) {
+        window.__hsReferrer = window.__hsReferrer || document.referrer;
+        var c = document.createElement("script");
+        c.src = "https://js.hs-analytics.net/analytics/1776963900000/47445192.js", c.type = "text/javascript", c.id = e;
+        var n = document.getElementsByTagName("script")[0];
+        n.parentNode.insertBefore(c, n)
+    }
+}("hs-analytics");
+
 ! function(t, e, r) {
     if (!document.getElementById(t)) {
         var n = document.createElement("script");
@@ -27,26 +57,17 @@
     "data-hsjs-env": "prod",
     "data-hsjs-hublet": "na1"
 });
-! function(e, t) {
-    if (!document.getElementById(e)) {
-        window.__hsReferrer = window.__hsReferrer || document.referrer;
-        var c = document.createElement("script");
-        c.src = "https://js.hs-analytics.net/analytics/1776962400000/47445192.js", c.type = "text/javascript", c.id = e;
-        var n = document.getElementsByTagName("script")[0];
-        n.parentNode.insertBefore(c, n)
-    }
-}("hs-analytics");
-
 ! function(t, e, r) {
     if (!document.getElementById(t)) {
         var n = document.createElement("script");
-        for (var a in n.src = "https://js.hsadspixel.net/pixels.js", n.type = "text/javascript", n.id = t, r) r.hasOwnProperty(a) && n.setAttribute(a, r[a]);
+        for (var a in n.src = "https://js.hubspotfeedback.com/feedbackweb-new.js", n.type = "text/javascript", n.id = t, r) r.hasOwnProperty(a) && n.setAttribute(a, r[a]);
         var i = document.getElementsByTagName("script")[0];
         i.parentNode.insertBefore(n, i)
     }
-}("hs-ads-pixel-47445192", 0, {
-    "data-ads-portal-id": 47445192,
-    "data-ads-env": "prod",
+}("hs-feedback-web-47445192", 0, {
+    "crossorigin": "anonymous",
+    "data-hubspot-feedback-portal-id": 47445192,
+    "data-hubspot-feedback-env": "prod",
     "data-loader": "hs-scriptloader",
     "data-hsjs-portal": 47445192,
     "data-hsjs-env": "prod",
@@ -71,32 +92,11 @@
 ! function(t, e, r) {
     if (!document.getElementById(t)) {
         var n = document.createElement("script");
-        for (var a in n.src = "https://js.hubspotfeedback.com/feedbackweb-new.js", n.type = "text/javascript", n.id = t, r) r.hasOwnProperty(a) && n.setAttribute(a, r[a]);
+        for (var a in n.src = "https://js.usemessages.com/conversations-embed.js", n.type = "text/javascript", n.id = t, r) r.hasOwnProperty(a) && n.setAttribute(a, r[a]);
         var i = document.getElementsByTagName("script")[0];
         i.parentNode.insertBefore(n, i)
     }
-}("hs-feedback-web-47445192", 0, {
-    "crossorigin": "anonymous",
-    "data-hubspot-feedback-portal-id": 47445192,
-    "data-hubspot-feedback-env": "prod",
-    "data-loader": "hs-scriptloader",
-    "data-hsjs-portal": 47445192,
-    "data-hsjs-env": "prod",
-    "data-hsjs-hublet": "na1"
-});
-var _hsp = window._hsp = window._hsp || [];
-_hsp.push(['addEnabledFeatureGates', []]);
-_hsp.push(['setBusinessUnitId', 0]);
-! function(t, e, r) {
-    if (!document.getElementById(t)) {
-        var n = document.createElement("script");
-        for (var a in n.src = "https://js.hs-banner.com/v2/47445192/banner.js", n.type = "text/javascript", n.id = t, r) r.hasOwnProperty(a) && n.setAttribute(a, r[a]);
-        var i = document.getElementsByTagName("script")[0];
-        i.parentNode.insertBefore(n, i)
-    }
-}("cookieBanner-47445192", 0, {
-    "data-cookieconsent": "ignore",
-    "data-hs-ignore": true,
+}("hubspot-messages-loader", 0, {
     "data-loader": "hs-scriptloader",
     "data-hsjs-portal": 47445192,
     "data-hsjs-env": "prod",
